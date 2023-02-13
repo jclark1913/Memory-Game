01de27dbd656513b090ddff099d7b186bbeff4e1
Justin Clark <jclarksummit@gmail.com>
2023-02-12 20:58:21 -0500

    Added basic game board + cards

--- memory-game.js
+++ memory-game.js
@@ -1,6 +1,78 @@
 // Generate cards for game board
 
-let cardsContainer = document.querySelector(".cards-container")
+let cardsContainer = document.querySelector(".cards-container");
+
+// Possible pairs
+let cards = [
+  [
+    { name: 'elephant', content: "<h2>فيل (فيلة)</h2>" },
+    { name: 'elephant', image: "./images/elephant.jpg" }
+  ],
+  [
+    { name: 'giraffe', content: "<h2>زرافة (-ات)</h2>" },
+    { name: 'giraffe', image: "./images/giraffe.jpg" }
+  ],
+  [
+    { name: 'monkey', content: "<h2>قرد (قرود)</h2>" },
+    { name: 'monkey', image: './images/monkey.jpg' }
+  ],
+  [
+    { name: 'cat', content: '<h2>قطة (قطط)</h2>' },
+    { name: 'cat', image: './images/cat.jpg' }
+  ],
+  [
+    { name: 'dog', content: '<h2>كلب (كلاب)</h2>' },
+    { name: 'dog', image: './images/dog.jpg' }
+  ],
+  [
+    { name: 'bird', content: '<h2>عصفور (عصافير)</h2>' },
+    { name: 'bird', image: './images/bird.jpg' }
+  ],
+  [
+    { name: 'ant', content: '<h2>نملة (نمل)</h2>' },
+    { name: 'ant', image: './images/ant.jpg' }
+  ],
+  [
+    { name: 'turtle', content: '<h2>سلحفاة (سلاحف)</h2>' },
+    { name: 'turtle', image: './images/turtle.jpg' }
+  ],
+  [
+    { name: 'lizard', content: '<h2>سحلية (سحالي)</h2>' },
+    { name: 'lizard', image: './images/lizard.jpg' }
+  ],
+  [
+    { name: 'horse', content: '<h2>خيل (خيول)</h2>' },
+    { name: 'horse', image: './images/horse.jpg'}
+  ]
+];
+
+// Select 10 pairs and shuffle them
+
+function selectPairs(pairArray) {
+  let resultArray = [];
+  for (let i = 0; i < 10; i++){
+    for (let x = 0; x < pairArray[i].length; x++) {
+      resultArray.push(pairArray[i][x]);
+    }
+  }
+  return resultArray;
+}
+
+let currentDeck = selectPairs(cards);
+
+// Shuffle function
+
+function shuffle(items) {
+  for (let i = items.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    [items[i], items[j]] = [items[j], items[i]];
+  }
+  return items;
+}
+
+console.log(currentDeck);
+let currentDeckShuffled = shuffle(currentDeck);
+console.log(currentDeckShuffled);
 
 // -- Generate divs according to difficulty parameters
 const normalWidth = 900 + 'px';
@@ -13,6 +85,16 @@ function initializeCardGrid(gridSizeRows, gridSizeCols) {
     cardsContainer.appendChild(memoryCard);
     cardsContainer.style.maxWidth = normalWidth;
     cardsContainer.style.maxHeight = normalHeight;
+    if (i < currentDeckShuffled.length) {
+      if (currentDeckShuffled[i].content) {
+        memoryCard.innerHTML = currentDeckShuffled[i].content;
+      } else {
+        let image = document.createElement("img");
+        image.src = currentDeckShuffled[i].image;
+        image.classList.add('image-card');
+        memoryCard.appendChild(image);
+      }
+    }
   }
 }
 
@@ -21,4 +103,11 @@ initializeCardGrid(4, 5);
 // Game initialization
 
 // -- Start button: this should populate each card with the pairs
+
+
+
+
+// Cards
+
+
 
